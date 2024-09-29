75d67d8b9e3e444cfc96f30185ad5600bd4bde0a
Ivan Aladyshkin <sanich1552@gmail.com>
2024-09-29 22:21:37 +0300

    Сделаны функции сортировки и фильтрации. Отформатирован код

--- src/processing.py
+++ src/processing.py
@@ -12,18 +12,8 @@ def filter_by_state(transactions: list, state='EXECUTED') -> list:
 def sort_by_date(transactions: list, sort_direction=True) -> list:
     """ Сортирует транзакции по дате выполнения """
 
-    for i in range(len(transactions)):
-        result_transactions = []
-        earliest_tran = transactions[0]
-        for transaction in transactions:
-            if transaction['date'] < earliest_tran['date']:
-                earliest_tran = transaction
-        result_transactions.append(earliest_tran)
-        transactions.remove(transaction)
-    if sort_direction:
-        return result_transactions
-    else:
-        return reversed(result_transactions)
+    transactions.sort(key=lambda transaction: transaction['date'], reverse=sort_direction)
+    return transactions
 
 
 test_list_1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
@@ -31,4 +21,4 @@ test_list_2 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:2
 
 print(filter_by_state(test_list_1, 'EXECUTED'))
 
-print(sort_by_date(test_list_2))
+print(sort_by_date(test_list_2, sort_direction=False))
