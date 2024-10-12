97c2cfebefc6389d2df8b874c9d4541e1262ae6a
Ivan Aladyshkin <sanich1552@gmail.com>
2024-10-12 14:19:50 +0300

    Сделаны тесты для masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,16 @@
 def get_mask_card_number(card_num: str) -> str:
     """Скрывает номер карты"""
-    card_num_mask = card_num[:4] + " " + card_num[4:6] + "**" + " " + "****" + " " + card_num[-4::1]
+    if len(card_num) == 16:
+        card_num_mask = card_num[:4] + " " + card_num[4:6] + "**" + " " + "****" + " " + card_num[-4::1]
+    else:
+        raise ValueError('Неправильный номер карты')
     return card_num_mask
 
 
 def get_mask_account(account_num: str) -> str:
     """Скрывает номер счета"""
-    account_num_mask = "**" + account_num[-4::1]
+    if len(account_num) == 20:
+        account_num_mask = "**" + account_num[-4::1]
+    else:
+        raise ValueError('Неправильный номер счета')
     return account_num_mask
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,26 @@ import pytest
 
 from src.masks import get_mask_card_number, get_mask_account
 
-@pytest.mark.parametrize('card_num, hide_num', [('1596837868705199', '1596 83** **** 5199'), ('7158300734726758', '7158 30** **** 6758')])
+
+@pytest.mark.parametrize(
+    "card_num, hide_num", [("1596837868705199", "1596 83** **** 5199"), ("7158300734726758", "7158 30** **** 6758")]
+)
 def test_get_mask_card_number(card_num, hide_num):
     assert get_mask_card_number(card_num) == hide_num
 
+def test_get_mask_card_number_raise():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number('')
+
+    assert str(exc_info.value) == "Неправильный номер карты"
+
+@pytest.mark.parametrize(
+    "account_num, hide_num", [("64686473678894779589", "**9589"), ("73654108430135874305", "**4305")]
+)
+def test_get_mask_account(account_num, hide_num):
+    assert get_mask_account(account_num) == hide_num
+
+def test_get_mask_account_raise():
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account('')
+    assert str(exc_info.value) == "Неправильный номер счета"
