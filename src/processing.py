11c1a6f1bc6665e66339df16307aba7a39eb4c5c
Ivan Aladyshkin <sanich1552@gmail.com>
2024-10-13 16:08:44 +0300

    готовы тесты для processing.py

--- src/processing.py
+++ src/processing.py
@@ -4,6 +4,8 @@ def filter_by_state(transactions: list, state: str = "EXECUTED") -> list:
 
     result_transactions = []
     for transaction in transactions:
+        if 'state' not in transaction.keys():
+            continue
         if transaction["state"] == state:
             result_transactions.append(transaction)
     return result_transactions
@@ -16,19 +18,4 @@ def sort_by_date(transactions: list, sort_direction: bool = True) -> list:
     return transactions
 
 
-test_list_1 = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-test_list_2 = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-print(filter_by_state(test_list_1, "EXECUTED"))
-
-print(sort_by_date(test_list_2, sort_direction=False))
+print(sort_by_date([{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}, {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},{"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}, {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},], sort_direction=False))
