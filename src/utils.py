b2f42b8167816543313667a2bb9dc418125b381e
Ivan Aladyshkin <sanich1552@gmail.com>
2025-09-08 00:27:30 +0300

    Домашка 12.1 сделана одна функция из двух - работа с json

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,24 @@
 import json
+import os
 def get_trans_list(filename: str) -> list:
     """Получает путь до JSON-файла и возвращает список словарей с транзакциями"""
-    with open(filename, 'r') as f:
-        trans_list = json.load(f)
-        return trans_list
 
 
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            trans_list = json.load(f)
+            return trans_list
+
+    except:
+        return list()
+
+
+print(get_trans_list(os.path.abspath("../data/operations.json")))
+print(type(get_trans_list(os.path.abspath("../data/operations.json"))))
 
 def count_trans_sum(transaction: dict):
     """Получает на вход транзакцию и выводит её сумму в рублях"""
 
     pass
+
+#print(os.path.abspath("../data/operations.json"))
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,24 @@
 from unittest.mock import Mock
 from unittest.mock import patch
 from src.utils import get_trans_list
+import os
 
-@patch('builtins.open', create=True)
-def test_get_trans_list(mock_open):
-    mock_file = mock_open.return_value.__enter__.return_value
-    mock_file.read.return_value = 'test data'
-    assert get_trans_list('test.txt') == 'test data'
-    mock_open.assert_called_once_with('test.txt', 'r')
+def test_get_trans_list():
+    assert get_trans_list(os.path.abspath("data/lol.json")) == []
+    assert get_trans_list(os.path.abspath("data/operations.json"))[0] == {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Maestro 1596837868705199",
+    "to": "Счет 64686473678894779589"
+  }
+
 
