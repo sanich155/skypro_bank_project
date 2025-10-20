ecbbbaa38f21423872e6c8626067ced3cf37c957
Ivan Aladyshkin <sanich1552@gmail.com>
2025-10-20 03:35:36 +0300

    форматирование

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,13 @@
 import json
 import os
 
+
 def get_trans_list(filename: str) -> list:
     """Получает путь до JSON-файла и возвращает список словарей с транзакциями"""
 
     try:
-        with open(os.path.abspath(filename), 'r', encoding='utf-8') as f:
+        with open(os.path.abspath(filename), "r", encoding="utf-8") as f:
             trans_list = json.load(f)
             return trans_list
-    except:
+    except Exception:
         return list()
